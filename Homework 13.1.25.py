223de59ac1726d1eafc532571d32dd2e939f5f05
amirkle27 <amirkle@gmail.com>
2025-01-17 21:46:52 +0200

    Update Homework 13.1.25.py

--- Homework 13.1.25.py
+++ Homework 13.1.25.py
@@ -1,4 +1,11 @@
 from datetime import datetime
+import sqlite3
+
+connection = sqlite3.connect("product.db")
+connection.row_factory = sqlite3.Row
+cursor = connection.cursor()
+
+
 class FoodProduct:
     def __init__(self, name:str, price:float, category:str, production_date:datetime, expiration_date:datetime):
         self.__name = name
@@ -12,8 +19,6 @@ class FoodProduct:
             return self.__price + other
         else:
             raise TypeError(f"Price must be of type int or {type(other)}")
-
-
 
     def __sub__(self, other):
         if isinstance(other, int):
@@ -150,24 +155,122 @@ class FoodProduct:
 
 
 
+milk = FoodProduct('Milk', 5, 'Dairy', datetime.strptime('16/1/2024', '%d/%m/%Y'), datetime.strptime('27/3/2026', '%d/%m/%Y'))
+cottage = FoodProduct('Cottage', 7, 'Dairy', datetime.strptime('15/3/2024', '%d/%m/%Y'), datetime.strptime('20/4/2024', '%d/%m/%Y'))
+chicken_breast = FoodProduct('Chicken Breast', 39.9, 'Meat', datetime.strptime('9/9/2024', '%d/%m/%Y'), datetime.strptime('1/2/2025', '%d/%m/%Y'))
+waffles = FoodProduct('Waffles', 14.5, 'Parve', datetime.strptime('13/1/2025', '%d/%m/%Y'), datetime.strptime('15/3/2025', '%d/%m/%Y'))
 
-first = FoodProduct('Milk', 5, 'Dairy', datetime.strptime('16/1/2024', '%d/%m/%Y'), datetime.strptime('27/3/2026', '%d/%m/%Y'))
-second = FoodProduct('Cottage', 7, 'Dairy', datetime.strptime('15/3/2024', '%d/%m/%Y'), datetime.strptime('20/4/2024', '%d/%m/%Y'))
-third = FoodProduct('Chicken Breast', 39.9, 'Meat', datetime.strptime('9/9/2024', '%d/%m/%Y'), datetime.strptime('1/2/2025', '%d/%m/%Y'))
+products_just_for_tryout = [milk,cottage, chicken_breast, waffles]
 
-print(first)
-print(second)
-print(first.price+second.price)
-print(first+1)
-# print(first+"banana")
 
-print(first.price-second.price)
-print(first==second)
-print(first>second)
-print(first<second)
-print(first.price*second.price)
-print(first!=second)
-print(hash(first))
-print(len(first))
-print(first)
-print(first.remaining)
+print(milk)
+print(cottage)
+print(milk.price+cottage.price)
+print(milk+1)
+print(milk.price-cottage.price)
+print(milk==cottage)
+print(milk>cottage)
+print(milk<cottage)
+print(milk.price*cottage.price)
+print(milk!=cottage)
+print(hash(milk))
+print(len(milk))
+print(milk)
+print(milk.remaining)
+print(waffles)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS FoodProducts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    price TEXT NOT NULL,
+    category TEXT NOT NULL,
+    production_date TEXT NOT NULL,
+    expiration_date TEXT NOT NULL)
+    """)
+connection.commit()
+
+
+def get_and_insert_product(curser):
+    while True:
+        new_product = input("Do you wish to add a new product?\n"
+                            "If yes, please enter: Product Name, price, category, production_date (dd/mm/yyyy), expiration_date (dd/mm/yyyy)\n"
+                            "Otherwise, type 'no' or 'n' to quit\n").strip()
+
+        if new_product.lower() in ("n", "no"):
+            print(f"Thank you\nGoodbye.")
+            break
+
+        else:
+            try:
+                product_data = new_product.split(",")
+                if len(product_data) != 5:
+                    print("Invalid input. Product must have 5 details: Product Name, price, category, production_date (dd/mm/yyyy), expiration_date (dd/mm/yyyy)\n"
+                          "Please try again")
+                    continue
+                name = str(product_data[0].strip())
+                price = float(product_data[1].strip())
+                category = str(product_data[2].strip())
+                production_date = datetime.strptime(product_data[3].strip(), '%d/%m/%Y')
+                expiration_date = datetime.strptime(product_data[4].strip(), '%d/%m/%Y')
+
+                product = FoodProduct(name, price, category, production_date, expiration_date)
+
+                existing_product = curser.execute("""
+                SELECT * FROM FoodProducts WHERE name = ?""", (name,)).fetchone()
+                if existing_product:
+                    print(f"Product {name} already exits in the database.")
+                else:
+                    insert_product(curser, product)
+            except Exception as e:
+                print(f"An error occurred with: {e}. Please try again")
+            show_table = input("Would you like to see the full products list?")
+            if show_table.lower() in ("n", "no"):
+                print("Thank you, goodbye")
+                break
+            else:
+                show_products(cursor)
+                
+def insert_product(curser, product):
+    query = """
+                        INSERT INTO FoodProducts (name, price, category, production_date, expiration_date) VALUES
+                        (?,?,?,?,?)"""
+    curser.execute(query,
+                   (product.name, product.price, product.category, product.production_date, product.expiration_date))
+    curser.connection.commit()
+    product_id = curser.execute("""SELECT id FROM FoodProducts WHERE name = ?""", (product.name,)).fetchone()
+    print(f"Product{product.name} added to db with id: {product_id[0]}")
+
+
+def insert_a_list_of_products(cursor,list):
+    for product in list:
+        insert_product(cursor, product)
+        print(f"{product}\n")
+
+
+def show_products(cursor):
+    products = cursor.execute("""SELECT * FROM FoodProducts""").fetchall()
+    for row in products:
+        product = FoodProduct(
+            name=row['name'],
+            price=row['price'],
+            category = row['category'],
+            production_date = datetime.strptime(row['production_date'], '%Y-%m-%d %H:%M:%S'),
+            expiration_date = datetime.strptime(row['expiration_date'], '%Y-%m-%d %H:%M:%S')
+        )
+        print(f"{product}\n")
+
+
+
+get_and_insert_product(cursor)
+
+show_products(cursor)
+
+insert_a_list_of_products(cursor,products_just_for_tryout)
+
+
+# def drop_table (cursor, table_to_drop):
+#     cursor.execute(f"DROP TABLE {table_to_drop}")
+#     print( f"table {table_to_drop} dropped")
+#
+# drop_table(cursor,'FoodProducts')
